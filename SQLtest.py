3bd8a3cecfdcec8c24f656880112a2560416ebe6
Yunfonium <52287745+Yunfonium@users.noreply.github.com>
2021-04-27 16:08:16 +0800

    Add files via upload

--- SQLtest.py
+++ SQLtest.py
@@ -10,29 +10,39 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-def create_a_default_table():
+def create_a_default_table(YearAndMonth):
     sql = "CREATE TABLE "
-    tb = 'default '
-    col = "(DATE date, GENRE nvarchar(max),COST float)"
+    tb = YearAndMonth
+    col = " (DATE date, COST float, GENRE nvarchar(max))"
     mycursor.execute(sql,tb,col)
 
 def drop_a_table(tablename):
     sql = 'DROP TABLE '
     mycursor.execute(sql,tablename)
 
-def edit_table_name(oldname,newname):
-    sql = 'RENAME TABLE ' + oldname + 'TO ' + newname
-    mycursor.execute(sql)
-
-def show_tables(tablename):
+def show_all_cost(tablename):
     sql = 'SELECT * FROM ' + tablename
     mycursor.execute(sql)
+    record = mycursor.fetchall()
+    return record
 
-def insert_new_spending():
-    pass
+def show_all_month():
+    sql = 'SELECT * FROM ALLTABLES'
+    mycursor.execute(sql)
+    myrecord = mycursor.fetchall()
+    return myrecord
+def show_the_month(month):
+    sql = 'SELECT * FROM ' + month
+    mycursor.execute(sql)
+    record = mycursor.fetchall()
+    return str(record)
 
-def set_date(now = date.datetime.now):
-    SELECT
+
+
+def insert_new_spending(Date = datetime.datetime.now , Genre = '' , Cost = 0):
+    sql = 'INSERT INTO default '
+    val = 'VALUE (' + Date + ',' + Genre + ',' + Cost + ')'
+    mycursor.execute(sql,val)
 
 
 
